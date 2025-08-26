9d1a169a72fd184f1fa76286d70b1b7a11a568fa
Felipe Lisandro <felipelisandro97@gmail.com>
2025-08-26 20:07:38 -0300

    Changed the way instructions send argumments.

--- include/instructions.h
+++ include/instructions.h
@@ -3,15 +3,17 @@
 
 #include "cpu.h"
 
-#define X(op, name, cycles, func, args) void func(CPU *cpu, void *args);
+typedef enum { REG_NONE = 0, REG_A = 1, REG_B = 2, REG_C = 3, REG_D = 4, REG_E = 5, REG_H = 6, REG_L = 7} Reg8;
+
+#define X(op, name, cycles, func, args) void func(CPU *cpu, int args);
 #include "instructions.def"
 #undef X
 
 typedef struct Instruction {
     char *mnemonic;
-    void (*execute)(CPU* cpu, void *args);
-    void *args;
     int cycles;
+    void (*execute)(CPU* cpu, int args);
+    int args;
 } Instruction;
 
 extern Instruction InstructionTable[256];
--- src/instructions.c
+++ src/instructions.c
@@ -1,18 +1,36 @@
 #include "instructions.h"
-#include "helper.h"
+#include "memory.h"
+#include "cpu_instructions.h"
+#include <stdio.h>
 
 #define X(op, name, cycles, func, args)[op] = {name, cycles, func, args},
-Instruction instruction_table[256] = {
+Instruction InstructionTable[256] = {
     #include "instructions.def"
 };
 #undef X
 
-void instr_NOP(CPU *cpu){
-
+uint8_t* get_8bit_reg(CPU* cpu, Reg8 r) {
+    switch(r){
+        case REG_NONE: return 0;
+        case REG_A: return &cpu->A;
+        case REG_B: return &cpu->B;
+        case REG_C: return &cpu->C;
+        case REG_D: return &cpu->D;
+        case REG_E: return &cpu->E;
+        case REG_H: return &cpu->H;
+        case REG_L: return &cpu->L;
+    }
+    return 0;
 }
 
-void instr_INC_8(CPU *cpu, void *args){
-    uint8_t *reg = (uint8_t *) args;
+void instr_NOP(CPU *cpu, int args){
+    return;
+}
+
+void instr_INC_8(CPU *cpu, int args){
+    Reg8 r = args;
+    uint8_t *reg = get_8bit_reg(cpu, r);
+    if(!reg) return;
     uint8_t old = *reg;
     (*reg)++;
     // zero flag
@@ -25,8 +43,10 @@ void instr_INC_8(CPU *cpu, void *args){
     else CLEAR_FLAG(cpu, FLAG_H);
 }
 
-void instr_DEC_8(CPU *cpu, void *args){
-    uint8_t *reg = (uint8_t *) args;
+void instr_DEC_8(CPU *cpu, int args){
+    Reg8 r = args;
+    uint8_t *reg = get_8bit_reg(cpu, r);
+    if(!reg) return;
     uint8_t old = *reg;
     (*reg)--;
     // zero flag
@@ -37,4 +57,60 @@ void instr_DEC_8(CPU *cpu, void *args){
     // if there's half carry, store
     if ((old & 0x0F) + 1 > 0x0F) SET_FLAG(cpu, FLAG_H);
     else CLEAR_FLAG(cpu, FLAG_H);
+}
+
+void instr_LD_8_U8(CPU *cpu, int args){
+    Reg8 r = args;
+    uint8_t *reg = get_8bit_reg(cpu, r);
+    if(!reg) return;
+    uint8_t imm = mem_read(cpu->PC++);
+    *reg = imm;
+}
+
+void instr_LD_A_8(CPU *cpu, int args){
+    Reg8 r = args;
+    uint8_t *reg = get_8bit_reg(cpu, r);
+    if(!reg) return;
+    cpu->A = *reg;
+}
+
+void instr_LD_B_8(CPU *cpu, int args){
+    Reg8 r = args;
+    uint8_t *reg = get_8bit_reg(cpu, r);
+    if(!reg) return;
+    cpu->B = *reg;
+}
+
+void instr_LD_C_8(CPU *cpu, int args){
+    Reg8 r = args;
+    uint8_t *reg = get_8bit_reg(cpu, r);
+    if(!reg) return;
+    cpu->C = *reg;
+}
+
+void instr_LD_D_8(CPU *cpu, int args){
+    Reg8 r = args;    uint8_t *reg = get_8bit_reg(cpu, r);
+    if(!reg) return;
+    cpu->D = *reg;
+}
+
+void instr_LD_E_8(CPU *cpu, int args){
+    Reg8 r = args;
+    uint8_t *reg = get_8bit_reg(cpu, r);
+    if(!reg) return;
+    cpu->E = *reg;
+}
+
+void instr_LD_H_8(CPU *cpu, int args){
+    Reg8 r = args;
+    uint8_t *reg = get_8bit_reg(cpu, r);
+    if(!reg) return;
+    cpu->H = *reg;
+}
+
+void instr_LD_L_8(CPU *cpu, int args){
+    Reg8 r = args;
+    uint8_t *reg = get_8bit_reg(cpu, r);
+    if(!reg) return;
+    cpu->L = *reg;
 }
