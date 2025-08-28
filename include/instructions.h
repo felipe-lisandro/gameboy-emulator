7dc557058d27890725807f6962ab44afbb65ce39
Felipe Lisandro <felipelisandro97@gmail.com>
2025-08-28 16:00:20 -0300

    Added a new enum for 16-bit registers

--- include/instructions.h
+++ include/instructions.h
@@ -4,6 +4,7 @@
 #include "cpu.h"
 
 typedef enum { REG_NONE = 0, REG_A = 1, REG_B = 2, REG_C = 3, REG_D = 4, REG_E = 5, REG_H = 6, REG_L = 7} Reg8;
+typedef enum { REG_NONE = 0, REG_BC = 1, REG_DE = 2, REG_HL = 3, REG_SP = 4} Reg16;
 
 #define X(op, name, cycles, func, args) void func(CPU *cpu, int args);
 #include "instructions.def"
