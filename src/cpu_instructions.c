d57bed706426316018fb7f6d6fe51645491930ec
Felipe Lisandro <felipelisandro97@gmail.com>
2025-08-30 14:15:51 -0300

    Removed unnecessary stubs

--- src/cpu_instructions.c
+++ src/cpu_instructions.c
@@ -3,27 +3,14 @@
 #include "memory.h"
 
 // missing instruction stubs for testing
-void instr_LD_BC_d16(CPU *cpu, int args) { return; }
-void instr_LD_BC_A(CPU *cpu, int args) { return; }
-void instr_INC_BC(CPU *cpu, int args) { return; }
 void instr_RLCA(CPU *cpu, int args) { return; }
 void instr_LD_a16_SP(CPU *cpu, int args) { return; }
-void instr_ADD_HL_BC(CPU *cpu, int args) { return; }
-void instr_LD_A_BC(CPU *cpu, int args) { return; }
-void instr_DEC_BC(CPU *cpu, int args) { return; }
 void instr_RRCA(CPU *cpu, int args) { return; }
-void instr_LD_DE_d16(CPU *cpu, int args) { return; }
-void instr_LD_DE_A(CPU *cpu, int args) { return; }
-void instr_INC_DE(CPU *cpu, int args) { return; }
 void instr_RLA(CPU *cpu, int args) { return; }
-void instr_JR_r8(CPU *cpu, int args) { return; }
-void instr_ADD_HL_DE(CPU *cpu, int args) { return; }
-void instr_LD_A_DE(CPU *cpu, int args) { return; }
-void instr_DEC_DE(CPU *cpu, int args) { return; }
-void instr_JR_NZ_r8(CPU *cpu, int args) { return; }
-void instr_LD_HL_d16(CPU *cpu, int args) { return; }
 void instr_LD_HLp_A(CPU *cpu, int args) { return; }
-void instr_INC_HL(CPU *cpu, int args) { return; }
 void instr_DAA(CPU *cpu, int args) { return; }
-void instr_ADD_HL_HL(CPU *cpu, int args) { return; }
 void instr_LD_A_HLp(CPU *cpu, int args) { return; }
+void instr_LD_SP_HL(CPU*cpu, int args){ return; }
+void instr_LD_SP_d16(CPU*cpu, int args){ return; }
+void instr_LD_HL_SP_r8(CPU*cpu, int args){ return; }
+void instr_ADD_SP_r8(CPU*cpu, int args){ return; }
